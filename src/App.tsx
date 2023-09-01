6d979893b1ede2bddf9a34da44940f55beea0635
Jong00 <jongchan0207@naver.com>
2023-09-01 16:42:12 +0900

    feat : route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 import './App.css';
+import Router from './Router';
 
-function App() {
+const App : FC = () => {
+
+  const [queryclient] = useState(
+    ()=>
+      new QueryClient ({
+        defaultOptions :{
+          queries :{
+            refetchOnWindowFocus : false,
+            retry : 0
+          },
+        },
+      })
+  );
+    
   return (
-    <div >
-
-    </div>
+    <RecoilRoot>
+      <QueryClientProvider client = {queryclient}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 }
 
