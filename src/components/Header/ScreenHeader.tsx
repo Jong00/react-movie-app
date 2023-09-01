daba54325f7d0a9fc37c097d435f193bf915b085
Jong00 <jongchan0207@naver.com>
2023-09-01 17:05:42 +0900

    commit 사진

---
+++ src/components/Header/ScreenHeader.tsx
@@ -0,0 +1,17 @@
+import {FC} from 'react';
+import styles from './ScreenHeader.module.css';
+
+interface ScreenHeaderProps{
+    pathname : string;
+}
+
+const ScreenHeader : FC<any> = ({
+
+}) => {
+    return (
+        <div></div>
+    )
+}
+
+export default ScreenHeader;
+
