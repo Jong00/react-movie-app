2cf25f4e6a79b27d387b64d696977eaeeca2d98c
Jong00 <jongchan0207@naver.com>
2023-09-08 11:44:36 +0900

    update2

--- src/Screens/MovieScreen/MovieContainer.tsx
+++ src/Screens/MovieScreen/MovieContainer.tsx
@@ -1,7 +1,64 @@
 import { Component, ReactNode } from "react";
 import MoviePresenter from "./MoviePresenter";
+import { moviesApi } from "../../api/movie";
 
-class MovieContainer extends Component<{}, any>{
+interface MovieContainerState{
+    nowPlaying: any[] | null;
+    upcoming: any[] | null;
+    popular: any[] | null;
+    topRated: any[] | null;
+    error: string | null;
+    loading: boolean;
+    popularInfinite: any[] | null;
+    nowPlayingInfinite: any[] | null;
+    upcomingInfinite: any[] | null;
+    topRatedInfinite: any[] | null;
+}
+
+class MovieContainer extends Component<{}, MovieContainerState>{
+
+    state: MovieContainerState = {
+        nowPlaying : null,
+        upcoming : null,
+        popular : null,
+        topRated : null,
+        error : null,
+        loading : true,
+        nowPlayingInfinite : null,
+        upcomingInfinite : null,
+        popularInfinite : null,
+        topRatedInfinite : null
+    };
+
+    async componentDidMount(){
+        try{
+            const {data : {resultes : nowPlaying}} = await moviesApi.nowPlaying();
+            const {data : {resultes : upcoming}} = await moviesApi.upcoming();
+            const {data : {resultes : popular}} = await moviesApi.popular();
+            const {data : {resultes : topRated}} = await moviesApi.topRated();
+            const {data : {resultes : popularInfinite}} = await moviesApi.popularInfinite();
+            const {data : {resultes : nowPlayingInfinite}} = await moviesApi.nowPlayingInfinite();
+            const {data : {resultes : upcomingInfinite}} = await moviesApi.upcomingInfinite();
+            const {data : {resultes : topRatedInfinite}} = await moviesApi.topRatedInfinite();
+
+            this.setState({
+                nowPlaying,
+                upcoming,
+                popular,
+                topRated,
+                popularInfinite,
+                nowPlayingInfinite,
+                upcomingInfinite,
+                topRatedInfinite,
+            });
+            
+        }catch{
+            this.setState({error : "영화 정보를 찾을 수 없습니다."});
+        }finally{
+            this.setState({loading : false});
+        }
+    }
+
     render(){
         return <MoviePresenter {...this.state} />
     }
--- src/Screens/MovieScreen/MoviePresenter.tsx
+++ src/Screens/MovieScreen/MoviePresenter.tsx
@@ -1,14 +1,44 @@
+import Loader from "../../components/Loader/Loader";
+
+interface MoviePresenterProps{
+    nowPlaying: any[] | null;
+    upcoming: any[] | null;
+    popular: any[] | null;
+    topRated: any[] | null;
+    error: string | null;
+    loading: boolean;
+    popularInfinite: any[] | null;
+    nowPlayingInfinite: any[] | null;
+    upcomingInfinite: any[] | null;
+    topRatedInfinite: any[] | null;
+}
 
 
 
+const MoviePresenter : React.FC<MoviePresenterProps>=({
+    nowPlaying,
+    upcoming,
+    popular,
+    topRated,
 
+    loading,
 
-const MoviePresenter : React.FC<any>=({
-
+    nowPlayingInfinite,
+    popularInfinite,
+    upcomingInfinite,
+    topRatedInfinite
 })=>{
-    return(
+
+    const {location : {pathname}} = window;
+
+    console.log(nowPlaying);
+
+    return loading ? (
+            <Loader/>
+        ):
+        (
         <div>무비 앱임</div>
-    );
+        );
 }
 
 export default MoviePresenter;
