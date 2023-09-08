ac1bf4ec0a266e74b553a42c90a6c17929405e27
Jong00 <jongchan0207@naver.com>
2023-09-08 10:22:16 +0900

    update1

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import {  Route, Routes, useLocation, useParams } from 'react-router-dom';
 import ScreenHeader from "./components/Header/ScreenHeader";
 import HomeContainer from './Screens/HomeScreen';
+import MovieContainer from './Screens/MovieScreen';
 
 
 const Router : FC = (props) => {
@@ -13,7 +14,8 @@ const Router : FC = (props) => {
     <div>
       <ScreenHeader pathname={pathname} />
       <Routes>
-        <Route path="/" Component={HomeContainer}></Route>
+          <Route path="/" Component={HomeContainer}></Route>
+          <Route path="/movie" Component={MovieContainer}></Route>
         </Routes>
         
       </div>
