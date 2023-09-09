7d7a952915fd29de0abd15a36990a94387023ded
Jong00 <jongchan0207@naver.com>
2023-09-09 11:30:05 +0900

    update 6

---
+++ src/Screens/DetailScreen/DetailContainer.tsx
@@ -0,0 +1,44 @@
+import { Component, ReactNode } from "react";
+import DetailPresenter from "./DetailPresenter";
+import { AnyMxRecord } from "dns";
+import { useParams } from "react-router-dom";
+import withRouter from "../../utils/withRouter";
+import { error } from "console";
+
+interface DetailContainerState{
+    result : any | null,
+    error : string | null,
+    loading : boolean,
+    recommandations : any,
+    cast : any,
+    keyword : any,
+    backdrops : any,
+    posters : any,
+    tvDetail2 : any,
+    reviews : any
+}
+
+class DetailContainer extends Component<{}, DetailContainerState>{
+    constructor(props : any){
+        super(props);
+        this.state = ({
+            result : [],
+            error : null,
+            loading : true,
+            recommandations : [null],
+            cast : [],
+            keyword : [],
+            reviews : [],
+            backdrops : [],
+            posters : [],
+            tvDetail2 : []
+        });
+    }
+
+
+    render(){
+        return <DetailPresenter/>
+    }
+};
+
+export default withRouter(DetailContainer);
